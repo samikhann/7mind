c42ae5d6cbb5868a96788a623c1d263d611ebbda
Ess Kay <msk@SK-MacBook-Pro.local>
2021-10-16 15:58:51 +0200

    Cleaning Code

--- src/Test.java
+++ src/Test.java
@@ -6,31 +6,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-//import org.testng.Assert;
 
 public class Test {
 	public static void main(String[] args) {
-//		System.out.println("aaa");
-//		System.setProperty("webdriver.chrome.driver", "/Users/msk/Downloads/chromedriver 2");
-//		System.setProperty("webdriver.chrome.driver", "/Users/msk/Downloads/chromedriver-2");
-
-//		System.out.println("11");
-//        ChromeDriver driver=new ChromeDriver();
-//        driver.manage().window().maximize();
-//        driver.get("https://www.linkedin.com/login");
-//        
-//        WebElement username=driver.findElement(By.id("username"));
-//        WebElement password=driver.findElement(By.id("password"));
-//        WebElement login=driver.findElement(By.xpath("//button[text()='Sign in']"));
-//        
-//        username.sendKeys("example@gmail.com");
-//        password.sendKeys("password");
-//        login.click();
-//        
-//        String actualUrl="https://www.linkedin.com/feed/";
-//        String expectedUrl= driver.getCurrentUrl();
-        
-//        Assert.assertEquals(expectedUrl,actualUrl);
+		
 		
 		System.setProperty("webdriver.gecko.driver","/Users/msk/Downloads/chromedriver-2");
     	WebDriver driver = new FirefoxDriver();
@@ -41,14 +20,14 @@ public class Test {
         WebElement username=driver.findElement(By.name("email"));
         WebElement password=driver.findElement(By.name("password"));
         WebElement login=driver.findElement(By.name("submit"));
-//        WebElement login=driver.findElement(By.xpath("//button[text()='Sign in']"));
+
         
 
 		username.sendKeys("EMAIL");
-		password.sendKeys("PASSWORD");
+		password.sendKeys("PASSWORD ");
 		login.click();
         
-//        driver.close();
+
         System.exit(0);
 	}
 }  
